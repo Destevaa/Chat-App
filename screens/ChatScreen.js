ab50c58df63f522141894b96abadbf3f5290c61a
darryl <darryl.esteva@gmail.com>
2019-07-25 15:27:26 +0800

    Working Chat Updates

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { SafeAreaView, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { SafeAreaView, View, Text, Dimensions, TextInput, TouchableOpacity } from 'react-native';
 import styles from '../constants/styles';
 import firebase from 'firebase';
 import User from '../User';
+import { FlatList } from 'react-native-gesture-handler';
+import { isProperty } from '@babel/types';
 
 export default class ChatScreen extends React.Component {
     static navigationOptions = ({ navigation }) => {
@@ -17,15 +19,26 @@ export default class ChatScreen extends React.Component {
             person: {
                 name: props.navigation.getParam('name'),
                 phone: props.navigation.getParam('phone'),
-
-
-            }
+            },
+            textMessage: '',
+            messageList: []
         }
     }
 
-    state = {
-        textMessage: ''
+    componentDidMount(){
+        firebase.database().ref('messages').child(User.phone).child(this.state.person.phone)
+        .on('child_added', (value)=>{
+            this.setState((prevState)=> {
+                return {
+                    messageList: [...prevState.messageList, value.val()]
+                }
+            })
+        })
     }
+
+    // state = {
+    //     textMessage: ''
+    // }
 
     handleChange = key => val => {
         this.setState({ [key]: val })
@@ -47,9 +60,38 @@ export default class ChatScreen extends React.Component {
         }
 
     }
+
+    renderRow = ({item}) => {
+        return(
+            <View style={{
+                flexDirection:'row',
+                width:'60%',
+                alignSelf: item.from===User.phone ? 'flex-end' : 'flex-start',
+                backgroundColor: item.from===User.phone ? '#00897b' : '#7cb342',
+                borderRadius:5,
+                marginBottom:10
+
+            }}>
+                <Text style={{color:'#fff',padding:7,fontSize:16}}>
+                    {item.message}
+                </Text>
+                <Text style={{color:'#eee',padding:3,fontSize:12}}>{item.time}</Text>
+
+
+            </View>
+        )
+    }
+
     render() {
+        let {height, width} = Dimensions.get('window');
         return (
             <SafeAreaView>
+                <FlatList
+                    style={{padding:10,height: height * 0.8}}
+                    data={this.state.messageList}
+                    renderItem={this.renderRow}
+                    keyExtractor={(item, index) => index.toString()}
+                />
                 <View style={{ flexDirection: 'row', alignItems: 'center' }}>
 
 
