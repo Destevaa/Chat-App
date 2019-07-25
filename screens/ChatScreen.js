4dacf519ce28fc5f0a4220998aaaa3f8d739831b
darryl <darryl.esteva@gmail.com>
2019-07-25 15:42:04 +0800

    Stable Release

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -44,6 +44,18 @@ export default class ChatScreen extends React.Component {
         this.setState({ [key]: val })
     }
 
+    convertTime = (time) => {
+        let d = new Date(time);
+        let c = new Date();
+        let result = (d.getHours < 10 ? '0' : '') + d.getHours() + ':';
+        result += (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
+
+        if(c.getDay() !== d.getDay()){
+            result = d.getDay() + ' ' + d.getMonth()  + ' ' + result;
+        }
+        return result;
+    }
+
     sendMessage = async () => {
         if (this.state.textMessage.length > 0) {
             let msgId = firebase.database().ref('messages').child(User.phone).child(this.state.person.phone).push().key;
@@ -75,7 +87,7 @@ export default class ChatScreen extends React.Component {
                 <Text style={{color:'#fff',padding:7,fontSize:16}}>
                     {item.message}
                 </Text>
-                <Text style={{color:'#eee',padding:3,fontSize:12}}>{item.time}</Text>
+                <Text style={{color:'#eee',padding:3,fontSize:12}}>{this.convertTime(item.time)}</Text>
 
 
             </View>
@@ -92,7 +104,7 @@ export default class ChatScreen extends React.Component {
                     renderItem={this.renderRow}
                     keyExtractor={(item, index) => index.toString()}
                 />
-                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <View style={{ flexDirection: 'row', alignItems: 'center', marginHorizontal:5}}>
 
 
                     <TextInput
@@ -101,7 +113,7 @@ export default class ChatScreen extends React.Component {
                         placeholder="Type Message.."
                         onChangeText={this.handleChange('textMessage')}
                     />
-                    <TouchableOpacity onPress={this.sendMessage}>
+                    <TouchableOpacity onPress={this.sendMessage} style={{paddingBottom:10,marginLeft:5}}>
                         <Text style={styles.btnText}>Send</Text>
                     </TouchableOpacity>
                 </View>
